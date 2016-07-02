1453ea4244df3d7a5a879300fdeb0d02fb5b2a95
koma <koma@mail.nksh.tp.edu.tw>
2016-07-02 11:59:21 +0800

    manual student

--- student/admin.py
+++ student/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from student.models import Assistant
+from student.models import Assistant, Work
 
 # Register your models here.
 admin.site.register(Assistant)
+admin.site.register(Work)
